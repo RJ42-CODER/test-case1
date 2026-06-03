47bb11c3cb96f2efcfff6ea32b967831d2dd14b1
RJ42-CODER <riyajamsutkar01@gmail.com>
2026-06-03 10:29:49 +0530

    added sub function

--- calculator.py
+++ calculator.py
@@ -5,4 +5,7 @@
 def add(a,b):
     return a - b
 
+def sub(a,b):
+    return a-b
+
 # print(add())
--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,13 @@
 import pytest
 
-from calculator import add
+from calculator import add, sub
 
 def test_add():
     assert add(4,5) == 9
     assert add(1,2) == 3
     assert add(0,0) == 0
 
+def test_sub():
+    assert sub(5,4) ==1
+    assert sub(2,1) == 1
+    assert sub(10,1) == 7
