18022bb16f504a8bcf66096820274d9d172dbcca
RJ42-CODER <riyajamsutkar01@gmail.com>
2026-06-03 11:32:40 +0530

    changes in code and added tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from calculator import add, sub
+from calculator import add, sub,mul,div
 
 def test_add():
     assert add(4,5) == 9
@@ -11,3 +11,13 @@ def test_sub():
     assert sub(5,4) ==1
     assert sub(2,1) == 1
     assert sub(10,1) == 9
+
+def test_mul():
+    assert mul(2,3)==6
+    assert mul(4,5)==20
+    assert mul(0,5)==0
+
+def test_div():
+    assert div(10,2)==5
+    assert div(20,4)==10
+    
